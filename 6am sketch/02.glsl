43e6b01df819b0a3a97823499bc815abb7a7fd1b
dain <57316771+DainPark-web@users.noreply.github.com>
2023-01-20 23:41:34 +0100

    update

--- 6am sketch/02.glsl
+++ 6am sketch/02.glsl
@@ -15,15 +15,26 @@ float Rect(vec2 uv, float left, float right, float bottom, float top, float blur
     return h * v;
 }
 
+float Merge(float a, float b){
+
+
+    return max(a, b);
+}
+
 
 void main(){
      vec2 uv = 2. *  gl_FragCoord.xy/iResolution.xy - 1.;
     uv.x *= iResolution.x/iResolution.y;
 
 
-
-    vec3 color = vec3(Rect(uv, -1., 1., -0.2 + sin(iTime + uv.x) * 0.4, 0.2 + sin(iTime + uv.x) * 0.4, 0.0002));
-
+    float f = 0.;
+    vec3 color = vec3(f);
+    for(int i = 0; i < 20; i++){
+        float v = (float(i) * 1.);
+        float r =  Rect(uv, -2., 2., -0.005 + sin(iTime + (uv.x * v)) * (v * 0.05), 0.005 + sin(iTime + (uv.x * v)) * (v * 0.05), abs(uv.x * 0.03) + 0.0001);
+        f = Merge(f, r);
+    }
+    color = vec3(f);
 
     gl_FragColor = vec4(color, 1.);
 }
